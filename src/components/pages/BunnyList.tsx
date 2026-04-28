b2ff995673f9ca3f59af0c9d640dfd7ea685d4cc
YUU <798395040@qq.com>
2026-04-28 18:52:24 +0900

    BunnyListのUIを調整

--- src/components/pages/BunnyList.tsx
+++ src/components/pages/BunnyList.tsx
@@ -1,5 +1,5 @@
 import { memo, type FC } from "react";
-import { Button, Card } from "react-bootstrap";
+import { Badge, Button, Card, Col, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 import buttonStyles from "../css/button.module.scss";
@@ -24,10 +24,10 @@ export const BunnyList: FC = memo(() => {
   return (
     <div>
       <h1>Bunny List</h1>
-      <div className="d-flex flex-wrap gap-3">
-        {/* 新規うさぎのプロフィールを登録するためのカード */}
-        <div>
-          <Card style={{ width: "13rem" }}>
+      {/* 新規うさぎのプロフィールを登録するためのカード */}
+      <Row xs={2} sm={2} md={3} lg={4} className="g-3">
+        <Col>
+          <Card>
             <div style={{ height: "180px" }} className="d-flex">
               <Card.Img
                 variant="top"
@@ -37,7 +37,7 @@ export const BunnyList: FC = memo(() => {
               />
             </div>
             <Card.Body>
-              <Card.Title>新しいうさぎは～</Card.Title>
+              <Card.Title>新しいうさぎは</Card.Title>
               <Card.Text>こちらになります₍ᐢᐢ₎</Card.Text>
 
               <Button
@@ -49,28 +49,35 @@ export const BunnyList: FC = memo(() => {
               </Button>
             </Card.Body>
           </Card>
-        </div>
+        </Col>
 
         {/* うさぎのプロフィールをカード形式で表示 */}
         {bunnyProfiles.map((bunny) => (
-          <div key={bunny.id}>
-            <Card style={{ width: "13rem" }}>
+          <Col key={bunny.id}>
+            <Card>
               <div style={{ height: "180px" }} className="d-flex">
                 <Card.Img
                   variant="top"
                   src={`https://bunny-profile.s3.ap-northeast-1.amazonaws.com/${bunny.img || "img/default.png"}`}
-                  style={{ width: "70%", objectFit: "contain" }}
+                  style={{ width: "90%", objectFit: "contain" }}
                   className="mx-auto my-3"
                 />
               </div>
 
               <Card.Body>
-                <Card.Title>
-                  {bunny.name}
-                  <span style={{ fontSize: "14px", color: "#dc3545" }}>
-                    {bunny.disabled && " (編集不可)"}
-                  </span>
+                <Card.Title className="d-flex align-items-center">
+                  {bunny.disabled && (
+                    <Badge
+                      bg="danger"
+                      className="flex-shrink-0" //省略防止
+                      style={{ fontSize: "12px" }}
+                    >
+                      編集不可
+                    </Badge>
+                  )}
+                  <span className="text-truncate">{bunny.name}</span>
                 </Card.Title>
+
                 <Card.Text className="text-truncate">
                   {bunny.description}
                 </Card.Text>
@@ -84,9 +91,9 @@ export const BunnyList: FC = memo(() => {
                 </Button>
               </Card.Body>
             </Card>
-          </div>
+          </Col>
         ))}
-      </div>
+      </Row>
     </div>
   );
 });
